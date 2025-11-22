5c175eaf776588a345deb455d03a1417e0449370
kcarbzz <rakion2r@gmail.com>
2025-11-21 20:40:34 -0500

    fixes

--- src/router/routes.js
+++ src/router/routes.js
@@ -11,7 +11,7 @@ const routes = [
   },
 
   // Always leave this as last one,
-  // but you can also remove it
+  // but you can also remove it .
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
